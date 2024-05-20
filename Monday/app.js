f97eebd4a2c6f12833d2c0295afad720b9410bc6
Umairsaleem321 <umair.hammas@gmail.com>
2024-05-20 16:51:11 +0500

    Monday-work

---
+++ Monday/app.js
@@ -0,0 +1,19 @@
+
+// let name = prompt("what is your name..");
+// console.log("Welcome to Web" + " " + name);
+
+
+
+
+// Let age = +prompt("Enter your Age")
+// if (age >= 18 ){
+//     console.log("You can apply CNIC");
+// }else{
+//     console.log("You are not Eligible")
+// }
+
+let userNum = +prompt("Enter Your Number")
+let randomNum = Math.random()
+randomNum = randomNum * 10;
+let random = Math.round(randomNum)
+console.log(random);
